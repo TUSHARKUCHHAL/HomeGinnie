4b87872609060477a7fa95a52618a427285c713e
HarshitGpt10 <gptharshit.01@gmail.com>
2025-04-05 19:40:14 +0530

    done

--- frontend/src/Pages/Dashboard/AdminDashboard/Campaigns/NotificationModal.jsx
+++ frontend/src/Pages/Dashboard/AdminDashboard/Campaigns/NotificationModal.jsx
@@ -27,6 +27,7 @@ const NotificationModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
+    // <div className="fixed inset-0 bg-slate-500/60 backdrop-blur-sm bg-opacity-50 z-50 flex items-center justify-center">
     <div className="fixed inset-0 bg-slate-500/60 backdrop-blur-sm bg-opacity-50 z-50 flex items-center justify-center">
       <div className="bg-white rounded-lg w-full max-w-md p-6">
         <div className="flex justify-between items-center mb-4">
